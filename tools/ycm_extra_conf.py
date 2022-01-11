639cf799feef4c18c4748f6328fdb62515deb463
Juli Wang <juli_880205@126.com>
2022-01-11 17:47:51 +0800

    [TOOL] update vimrc and ycm_extra_cong.py

--- tools/ycm_extra_conf.py
+++ tools/ycm_extra_conf.py
@@ -1,6 +1,8 @@
 import os
 import ycm_core
- 
+
+home_dir = '/home/ubuntu/self-motiveted/'
+
 flags = [
     '-Wall',
     '-Wextra',
@@ -21,7 +23,31 @@ flags = [
     '-isystem',
     '/usr/include/c++/5',
     '-isystem',
-    '/usr/include/c++/5/bits'
+    '/usr/include/c++/5/bits',
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/nemu/include'),
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/nemu/src/isa/riscv32/include'),
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/nemu/src/isa/riscv64/include'),
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/nemu/tools/kvm-diff/include'),
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/nemu/tools/qemu-diff/include'),
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/abstract-machine/am/include'),
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/abstract-machine/am/src/platform/nemu/include'),
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/abstract-machine/klib/include'),
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/am-kernels/benchmarks/coremark/include'),
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/am-kernels/benchmarks/microbench/include'),
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/am-kernels/tests/am-tests/include'),
+    '-isystem',
+    os.path.join(home_dir, 'ics2021/am-kernels/tests/cpu-tests/include')
   ]
  
 SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', ]
