1d08313a50fd56e6da3851cd3f9760ec33055603
Juli Wang <juli_880205@126.com>
2022-01-22 18:18:40 +0800

    [NJU] Finish cpu test bit.c

--- ics2021/nemu/src/isa/riscv32/include/isa-all-instr.h
+++ ics2021/nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,7 +1,12 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap) \
-  f(addi) f(auipc) f(jal) f(jalr) f(add) f(sub) f(sltiu) f(bne) \
-  f(beq) f(sltu) f(xor) f(or)
+#define INSTR_LIST(f) f(lui) f(inv) f(nemu_trap) \
+  f(addi) f(slti) f(sltiu) f(xori) f(ori) f(andi) f(slli) f(srli) f(srai)\
+  f(auipc) f(jal) f(jalr) f(bne) \
+  f(beq) f(sb) f(sh) f(sw) f(sd) \
+  f(lb) f(lbu) f(lh) f(lhu) f(lw)\
+  f(lwu) f(ld) f(add) f(sub) f(sll) f(slt) f(sltu)\
+  f(xor) f(srl) f(sra) f(or) f(and) 
+
 def_all_EXEC_ID();
--- ics2021/nemu/src/isa/riscv32/instr/compute.h
+++ ics2021/nemu/src/isa/riscv32/instr/compute.h
@@ -6,72 +6,101 @@ def_EHelper(lui) {
 
 def_EHelper(addi) {
   rtl_addi(s, ddest, id_src1->preg, id_src2->imm);
-  //Log("\naddi: ");
-  //isa_reg_display();
+}
+
+def_EHelper(slti) {
+  rtl_setrelopi(s, RELOP_LT, ddest, id_src1->preg, id_src2->imm);
+}
+
+def_EHelper(sltiu) {
+  rtl_setrelopi(s, RELOP_LTU, ddest, id_src1->preg, id_src2->imm);
+}
+
+def_EHelper(xori) {
+  rtl_xori(s, ddest, id_src1->preg, id_src2->imm);
+}
+
+def_EHelper(ori) {
+  rtl_ori(s, ddest, id_src1->preg, id_src2->imm);
+}
+
+def_EHelper(andi) {
+  rtl_andi(s, ddest, id_src1->preg, id_src2->imm);
+}
+
+def_EHelper(slli) {
+  rtl_slli(s, ddest, id_src1->preg, id_src2->imm);
+}
+
+def_EHelper(srli) {
+  rtl_srli(s, ddest, id_src1->preg, id_src2->imm);
+}
+
+def_EHelper(srai) {
+  rtl_srai(s, ddest, id_src1->preg, id_src2->imm);
 }
 
 def_EHelper(auipc) {
   rtl_addi(s, ddest, &s->pc, id_src1->imm);
-  //Log("\nauipc: ");
-  //isa_reg_display();
 }
 
 def_EHelper(jal) {
   rtl_li(s, ddest, s->snpc);
-  //rtl_j(s, id_src1->imm);
   rtl_addi(s, &s->pc, &s->pc, id_src1->imm);
   rtl_j(s, s->pc);
-
-  //Log("\njal: ");
-  //isa_reg_display();
 }
 
 def_EHelper(jalr) {
   rtl_li(s, ddest, s->snpc);
   rtl_addi(s, &s->pc, id_src1->preg, id_src2->imm);
   rtl_j(s, s->pc);
-  //Log("\njalr: ");
-  //isa_reg_display();
-}
-
-def_EHelper(sltiu) {
-  rtl_setrelopi(s, RELOP_LTU, ddest, id_src1->preg, id_src2->imm);
-  //Log("\nstlui: ");
-  //isa_reg_display();
 }
 
 def_EHelper(add) {
   rtl_add(s, ddest, id_src1->preg, id_src2->preg);
-  //Log("\nadd: ");
-  //isa_reg_display();
 }
 
 def_EHelper(sub) {
   rtl_sub(s, ddest, id_src1->preg, id_src2->preg);
-  //Log("\nsub: ");
-  //isa_reg_display();
 }
 
-def_EHelper(bne) { 
-  rtl_jrelop(s, RELOP_NE, id_src1->preg, id_src2->preg, id_dest->imm);
-  //Log("\nbne: ");
-  //isa_reg_display();
+def_EHelper(sll) {
+  rtl_sll(s, ddest, id_src1->preg, id_src2->preg);
 }
 
-def_EHelper(beq) {
-  rtl_jrelop(s, RELOP_EQ, id_src1->preg, id_src2->preg, id_dest->imm);
-  //Log("\nbeq: ");
-  //isa_reg_display();
+def_EHelper(slt) {
+  rtl_setrelop(s, RELOP_LT, ddest, id_src1->preg, id_src2->preg);
 }
 
 def_EHelper(sltu) {
-  rtl_setrelop(s, RELOP_LTU, ddest, id_src1->preg, id_src2->preg); 
+  rtl_setrelop(s, RELOP_LTU, ddest, id_src1->preg, id_src2->preg);
 }
 
 def_EHelper(xor) {
   rtl_xor(s, ddest, id_src1->preg, id_src2->preg);
 }
 
+def_EHelper(srl) {
+  rtl_srl(s, ddest, id_src1->preg, id_src2->preg);
+}
+
+def_EHelper(sra) {
+  rtl_sra(s, ddest, id_src1->preg, id_src2->preg);
+}
+
 def_EHelper(or) {
   rtl_or(s, ddest, id_src1->preg, id_src2->preg);
 }
+
+def_EHelper(and) {
+  rtl_and(s, ddest, id_src1->preg, id_src2->preg);
+}
+
+def_EHelper(bne) { 
+  rtl_jrelop(s, RELOP_NE, id_src1->preg, id_src2->preg, id_dest->imm);
+}
+
+def_EHelper(beq) {
+  rtl_jrelop(s, RELOP_EQ, id_src1->preg, id_src2->preg, id_dest->imm);
+}
+
