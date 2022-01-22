957c7739878ef19745d42833c8695e1872ce8adc
Juli Wang <juli_880205@126.com>
2022-01-22 18:34:34 +0800

    [NJU] Finish cpu test bubble-sort

--- ics2021/nemu/src/isa/riscv32/include/isa-all-instr.h
+++ ics2021/nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -3,8 +3,9 @@
 
 #define INSTR_LIST(f) f(lui) f(inv) f(nemu_trap) \
   f(addi) f(slti) f(sltiu) f(xori) f(ori) f(andi) f(slli) f(srli) f(srai)\
-  f(auipc) f(jal) f(jalr) f(bne) \
-  f(beq) f(sb) f(sh) f(sw) f(sd) \
+  f(auipc) f(jal) f(jalr)\
+  f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu) \
+  f(sb) f(sh) f(sw) f(sd) \
   f(lb) f(lbu) f(lh) f(lhu) f(lw)\
   f(lwu) f(ld) f(add) f(sub) f(sll) f(slt) f(sltu)\
   f(xor) f(srl) f(sra) f(or) f(and) 
--- ics2021/nemu/src/isa/riscv32/instr/compute.h
+++ ics2021/nemu/src/isa/riscv32/instr/compute.h
@@ -96,11 +96,30 @@ def_EHelper(and) {
   rtl_and(s, ddest, id_src1->preg, id_src2->preg);
 }
 
+def_EHelper(beq) {
+  rtl_jrelop(s, RELOP_EQ, id_src1->preg, id_src2->preg, id_dest->imm);
+}
+
 def_EHelper(bne) { 
   rtl_jrelop(s, RELOP_NE, id_src1->preg, id_src2->preg, id_dest->imm);
 }
 
-def_EHelper(beq) {
-  rtl_jrelop(s, RELOP_EQ, id_src1->preg, id_src2->preg, id_dest->imm);
+def_EHelper(blt) { 
+  rtl_jrelop(s, RELOP_LT, id_src1->preg, id_src2->preg, id_dest->imm);
 }
+
+def_EHelper(bge) {
+  rtl_jrelop(s, RELOP_GE, id_src1->preg, id_src2->preg, id_dest->imm);
+}
+
+def_EHelper(bltu) { 
+  rtl_jrelop(s, RELOP_LTU, id_src1->preg, id_src2->preg, id_dest->imm);
+}
+
+def_EHelper(bgeu) {
+  rtl_jrelop(s, RELOP_GEU, id_src1->preg, id_src2->preg, id_dest->imm);
+}
+
+
+
 
