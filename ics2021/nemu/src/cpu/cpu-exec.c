cfbf71e3efee4a4f078f55d2ef94248d84d852a3
Juli Wang <juli_880205@126.com>
2022-02-19 15:12:21 +0800

    [NJU] Fix check watchpoint bug
    Kconfig
    src/cpu/cpu-exec.c

--- ics2021/nemu/src/cpu/cpu-exec.c
+++ ics2021/nemu/src/cpu/cpu-exec.c
@@ -20,6 +20,7 @@ rtlreg_t tmp_reg[4];
 
 void device_update();
 void fetch_decode(Decode *s, vaddr_t pc);
+extern void check_watchpoint();
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
