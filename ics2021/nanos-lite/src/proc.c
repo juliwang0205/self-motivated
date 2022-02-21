f5ed3949a34b5a0d0ca6364764cbed806a1b3648
Juli Wang <juli_880205@126.com>
2022-02-21 17:08:56 +0800

    [NJU] loader:read elf file
    nanos-litie/include/common.h
    nanos-lite/rc/loader.c
    nanos-lite/rc/proc.c

--- ics2021/nanos-lite/src/proc.c
+++ ics2021/nanos-lite/src/proc.c
@@ -2,6 +2,7 @@
 
 #define MAX_NR_PROC 4
 
+extern void naive_uload(PCB *pcb, const char *filename);
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
@@ -21,7 +22,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   switch_boot_pcb();
-
+  naive_uload(NULL, NULL);
   Log("Initializing processes...");
 
   // load program here
