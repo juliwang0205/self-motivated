71458b354def13d8e73b617289fc64beb125d520
Juli Wang <juli_880205@126.com>
2022-03-07 11:10:52 +0800

    [NJU] implement Nslider(NJU slider)
    modefiled:
       nanos-lite/src/proc.c
       navy-apps/Makefile
       navy-apps/apps/nslider/slides/convert.sh
       navy-apps/apps/nslider/src/main.cpp
       navy-apps/libs/libminiSDL/src/event.c
       navy-apps/libs/libminiSDL/src/general.c
       navy-apps/libs/libminiSDL/src/video.c

--- ics2021/navy-apps/libs/libminiSDL/src/event.c
+++ ics2021/navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -8,27 +9,108 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
+static char key_buf[64], *key_action, *key_key;
+static uint8_t key_state[sizeof(keyname) / sizeof(keyname[0])] = {0};
+//To Be Fast
+static int inline read_keyinfo(uint8_t *type, uint8_t *sym){
+  int ret = NDL_PollEvent(key_buf, sizeof(key_buf));
+  if (!ret){
+    return 0;
+  }
+  //printf("%s\n", key_buf);
+  key_action = key_buf;
+  int i;
+  for (i = 0; key_buf[i] != ' '; i++){}
+  key_buf[i] = '\0';
+  key_key = &key_buf[i + 1];
+
+  //截断\n
+  for (i = 0;  key_key[i] != '\0' && key_key[i] != '\n'; i++){}
+  if (key_key[i] == '\n'){
+    key_key[i] = '\0';
+  }
+
+  //strcmp("kd", key_action) == 0
+  if (key_action[1] == 'd'){//加速！！
+    *type = SDL_KEYDOWN;
+  }else{
+    *type = SDL_KEYUP;
+  }
+
+  for (i = 0; i < sizeof(keyname) / sizeof(char *); ++i){
+    //剪枝掉很多
+    if (key_key[0] == keyname[i][0] && strcmp(key_key, keyname[i]) == 0){
+      *sym = i;
+      //printf("%d %d\n", *type, *sym);
+      return ret;
+    }
+  }
+}
+
 int SDL_PushEvent(SDL_Event *ev) {
-  panic("SDL_PushEvent hasn't been implemented\n");
+  //panic("SDL_PushEvent hasn't been implemented\n");
   return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  panic("SDL_PollEvent hasn't been implemented\n");
-  return 0;
+  uint8_t type = 0, sym = 0;
+  // SDL_PumpEvents();
+
+
+  // if (pop(&type, &sym)){
+  //printf("SDL_PollEvent\n");
+  if (read_keyinfo(&type, &sym)){
+    ev->type = type;
+    ev->key.keysym.sym = sym;
+
+    switch(type){
+    case SDL_KEYDOWN:
+      key_state[sym] = 1;
+      //printf("%d Down\n", (int)sym);
+      break;
+
+    case SDL_KEYUP:
+      key_state[sym] = 0;
+      //printf("%d Up\n", (int)sym);
+      break;
+    }
+  }else {
+    return 0;
+  }
+  return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  panic("SDL_WaitEvent hasn't been implemented\n");
+  uint8_t type = 0, sym = 0;
+  //SDL_PumpEvents();
+
+  //while (!pop(&type, &sym)){
+  while (!read_keyinfo(&type, &sym)){
+    //SDL_PumpEvents();
+  }
+
+  event->type = type;
+  event->key.keysym.sym = sym;
+
+  switch(type){
+    case SDL_KEYDOWN:
+      key_state[sym] = 1;
+      break;
+
+    case SDL_KEYUP:
+      key_state[sym] = 0;
+      break;
+  }
+  //panic("SDL_WaitEvent hasn't been implemented\n");
   return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-  panic("SDL_PeepEvents hasn't been implemented\n");
+  //panic("SDL_PeepEvents hasn't been implemented\n");
   return 0;
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  panic("SDL_GetKeyState hasn't been implemented\n");
+  //panic("SDL_GetKeyState hasn't been implemented\n");
   return NULL;
 }
